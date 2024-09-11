e2b7b18e387f316e1e6cf8d07745a7cf3c6a50eb
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-11 23:03:32 +1000

    update java buttons

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ function handleclick() {
 let disablebuttonElement = document.querySelector("#disable-button");
 disablebuttonElement.addEventListener("click", handleclick);
 
-function handleclick() {
-  alert("hey!");
+function handleGreenclick() {
+  alert("hey, its green now!");
+  greenbuttonElement.style.backgroundColor = "green";
 }
 let greenbuttonElement = document.querySelector("#green-button");
-greenbuttonElement.addEventListener("click", handleclick);
-greenbuttonElement.style.color = "green";
+greenbuttonElement.addEventListener("click", handleGreenclick);
+
+function handleRedclick() {
+  alert("hey, its red now!");
+  redbuttonElement.classList.add("danger");
+}
+let redbuttonElement = document.querySelector("#red-button");
+redbuttonElement.addEventListener("click", handleRedclick);
